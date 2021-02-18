0d0ea1f8c4377c6df6fdd515535dc391298f902a
takanori-hagiwara <allhappyhagi@outlook.com>
2021-02-18 10:36:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+  
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :price, :image, :description, :category_id, :condition_id, :delivery_fee_id, :shipment_area_id, :shipment_date_id).merge(user_id: current_user.id)
+  end
+
 end
