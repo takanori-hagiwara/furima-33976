74e7949971d07c1b034d61bff2d06d6c1f7f41ff
takanori-hagiwara <allhappyhagi@outlook.com>
2021-02-26 17:01:46 +0900

    テストコードの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!
+  before_action :contributor_confirmation
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -7,28 +9,35 @@ class OrdersController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-     @order_address = OrderAddress.new(order_params)
-     if @order_address.valid?
-        pay_item
-        @order_address.save
-       redirect_to root_path
-     else
-       render :index
-     end
-   end
-  
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :shipment_area_id, :city, :street_address, :building, :phone).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :shipment_area_id, :city, :street_address, :building, :phone).merge(
+      user_id: current_user.id, token: params[:token], item_id: params[:item_id]
+    )
   end
-    def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+  def contributor_confirmation
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user == @item.user
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,18 +2,20 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :shipment_area_id, :city, :street_address, :building, :phone, :item_id, :user_id, :token, :order_id
 
-  with_options presence: true do 
+  with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :street_address
     validates :shipment_area_id, numericality: { other_than: 1 }
-    validates :phone, format: {with: /\A\d{11}\z/}
+    validates :phone, format: { with: /\A\d{11}\z/ }
+    validates :token
   end
-  
+
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, shipment_area_id: shipment_area_id, city: city, street_address: street_address, building: building, phone: phone, order_id: order.id)
+    Address.create(postal_code: postal_code, shipment_area_id: shipment_area_id, city: city, street_address: street_address,
+                   building: building, phone: phone, order_id: order.id)
   end
 end
